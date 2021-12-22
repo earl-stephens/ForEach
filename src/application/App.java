5fa816e3453697e521cb04b3201d0deba2ad93d3
earl-stephens <stephensearl@hotmail.com>
2021-12-22 16:22:22 -0700

    Show use of forEach loop

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,17 @@ package application;
 
 public class App {
 	public static void main(String[] args) {
+		//another way of iterating through an array
+		//very common
 		
+		String[] fruits = {"banana", "pear", "apple", "grape"};
+		
+		/* Sets the variable fruit and then assigns the variable
+		 * to each element in the fruits array.
+		 * This method does not give you the index of the array
+		 */
+		for(String fruit: fruits) {
+			System.out.println(fruit);
+		}
 	}
 }
