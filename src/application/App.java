68c030ae9e5297b9f613e6040a01c090942d0d12
earl-stephens <stephensearl@hotmail.com>
2021-12-22 16:35:47 -0700

    Complete the coding challenge

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
 		//another way of iterating through an array
@@ -14,5 +16,35 @@ public class App {
 		for(String fruit: fruits) {
 			System.out.println(fruit);
 		}
+		
+		/* Coding challenge
+		 * Write a program that gets three numbers input by the user (keep 
+		 * asking until they have entered exactly 3 numbers),
+		 * prints the numbers entered, prints the total of the numbers
+		 * 
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+		int arrayLength = 3;
+		int[] userInput = new int[arrayLength];
+		
+		System.out.println("Enter 3 integers below.");
+		
+		for(int i = 0; i < arrayLength; i++) {
+			System.out.println("Enter a number > ");
+			userInput[i] = scanner.nextInt();
+		}
+		
+		scanner.close();
+		
+		int total = 0;
+		System.out.println("The numbers you entered are: ");
+		
+		for(int numbers: userInput) {
+			System.out.println(numbers);
+			total += numbers;
+		}
+		
+		System.out.printf("The sum of the numbers is: %d", total);
 	}
 }
